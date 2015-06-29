d47dd3df4a0beb390009bb05e065c5c6b71be82a
Antonio Terceiro <terceiro@softwarelivre.org>
2015-06-29 11:39:05 -0300

    1.10.1 Release

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ EXCLUDE_FROM_PACKAGES = []
 
 setup(
     name='colab',
-    version='1.10',
+    version='1.10.1',
     url='https://github.com/colab-community/colab',
     author='Sergio Oliveira',
     author_email='sergio@tracy.com.br',
